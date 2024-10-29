f750094c57a4595c959116aa7446c6784ab8d882
amntllhz <amanatullah1904@gmail.com>
2024-10-30 00:49:54 +0700

    layout: fix hello interface

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -3,7 +3,7 @@
 <section class= "max-w-6xl mx-auto">
     <div class="flex rounded-2xl shadow-md bg-white p-6 mt-16 ring-1 ring-inset ring-blue-200 items-center">
 
-        <div class="w-1/2">
+        <div class="w-1/2 ml-6">
 
             {{-- badge --}}
             <div class="small-badge w-fit gap-x-3 flex bg-blue-50 rounded-full py-1.5 px-4 items-center ring-2 ring-inset ring-prim/10 mb-2">
@@ -19,12 +19,12 @@
 
             {{-- headline --}}
             <div class="flex flex-col mb-6">
-                <p class="text-5xl font-extrabold text-blue-700 leading-tight mb-2">A Laravel Project <br> With C-R-U-D Features</p>
-                <p class="max-w-md text-base text-blue-400">"Laravel project with user-friendly Create, Read, Update, and Delete features, plus a handy search function!"</p>
+                <p class="text-5xl font-extrabold text-slate-800 leading-tight mb-2">A Laravel Project <br> With <span class="text-blue-700">C</span>-<span class="text-blue-700">R</span>-<span class="text-blue-700">U</span>-<span class="text-red-600">D</span> Features</p>
+                <p class="max-w-md text-base text-gray-600">"Laravel project with user-friendly Create, Read, Update, and Delete features, plus a handy search function!"</p>
             </div>
 
             {{-- link --}}
-            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-xl bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+            <a href="{{ route('home') }}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-xl bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Start
                 <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
