a49233ede2ab07bdbc00f78b5aa2fa83fdd96590
amntllhz <amanatullah1904@gmail.com>
2024-11-03 19:36:38 +0700

    set: fix language

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,13 +72,13 @@
                         <div id="actionsDropdown" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="actionsDropdownButton">
                                 <li>
-                                    <a href="#" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Tambah Data</a>
+                                    <a href="#" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Insert Data</a>
                                 </li>
                             </ul>
                             <form action="/logout" method="POST">
                                 @csrf
                                 <button id="logout" name="logout" type="submit" class="text-gray-700 text-left w-full text-sm py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                                    Keluar
+                                    Get Out
                                 </button>
                             </form>
                         </div>                            
@@ -195,7 +195,7 @@
                                         {{-- modal footer --}}
                                         <div class="flex justify-end items-center">                                                                                
                                             <button data-modal-toggle="view-{{ $item->nim }}" type="button" class="text-white inline-flex items-center bg-prim hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">                                            
-                                                Kembali
+                                                Back
                                             </button>
                                         </div>
                                     </div>
@@ -296,7 +296,7 @@
 
                 <div class="p-5 space-x-4 flex items-center justify-between">
                     <div>
-                        <span class="bg-indigo-100 text-prim text-xs font-medium me-2 px-5 py-2 rounded-md dark:bg-blue-900 dark:text-blue-300">{{ $data->count() }} dari {{ $data->total() }} data</span>                        
+                        <span class="bg-indigo-100 text-prim text-xs font-medium me-2 px-5 py-2 rounded-md dark:bg-blue-900 dark:text-blue-300">{{ $data->count() }} from {{ $data->total() }} data</span>                        
                     </div>                                       
                     <div>
                         {{ $data->links() }} <!-- Link pagination -->
